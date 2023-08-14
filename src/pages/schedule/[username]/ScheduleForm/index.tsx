542f944e820199b4f9fece86e5dc0aaddcce000d
Lucas Macedo <lucasmdc57@gmail.com>
2023-08-14 07:35:54 -0300

    feat: creating schedule confirmation

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,6 +1,21 @@
+import { useState } from 'react'
 import { CalendarStep } from './CalendarStep'
-// import { ConfirmStep } from './ConfirmeStep'
+import { ConfirmStep } from './ConfirmeStep'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
+
+  function handleCancelConfirmation() {
+    setSelectedDateTime(null)
+  }
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmStep
+        schedulingDate={selectedDateTime}
+        onCancelConfirmation={handleCancelConfirmation}
+      />
+    )
+  }
+  return <CalendarStep onSelectDateTime={setSelectedDateTime} />
 }
